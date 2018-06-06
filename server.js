f9d8c3df0fe124c81fa9d9ebfc272ae00ad6c3f1
Stewart Boyd <stewartboyd1988@gmail.com>
2018-06-05 22:24:17 -0400

    s45

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
+
 
 const pageName = "Stewart Boyd";
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    fs.writeFile(__dirname + "/output.txt", log, {flag: "a"}, (err) => {
+        if (err) throw err;
+        console.log('file written too');
+        next();
+    })
+})
+
+app.use((req, res, next) => {
+    res.render("maintenance.hbs");
+})
+
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("screamIt", (element) => element.toUpperCase());
+
 app.get("/", (req, res) => {
 
     //res.send('<h1>hello express</h1>');
     var content = {pageName: "Stewart Boyd",
-        currentYear: new Date().getFullYear(),
         welcomeMessage: "Good to meet you"};
     res.render("home.hbs", content);
 });
@@ -22,8 +40,7 @@ app.get("/about", (req, res) => {
     // Render allows your to render using your view engine 
     // a view. Views for view engines are by default looked for
     // in the view directory
-    res.render('about.hbs', {pageName: "Stewart Boyd",
-        currentYear: new Date().getFullYear()});
+    res.render('about.hbs', {pageName: "Stewart Boyd"});
 });
 
 app.get("/bad", (req, res) => {
